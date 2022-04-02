d9572e23ea4a9f3af8733f187c0a71480a0e9df8
TyHulet <tyhulet@byui.edu>
2022-04-02 13:39:17 -0600

    main JS

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
 }
 
 //2. RAW TEXT STRINGS
@@ -26,33 +26,33 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
-let newStory = storyText;
+    let newStory = storyText;
 
-let xItem = randomValueFromArray(insertX);
-let yItem = randomValueFromArray(insertY);
-let zItem = randomValueFromArray(insertZ);
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replace(':insertx:', xItem);
-newStory = newStory.replace(':insertx:', xItem);
-newStory = newStory.replace(':inserty:',yItem);
-newStory = newStory.replace(':insertz:',zItem);
-
-
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace('Bob', name);
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round((94 - 32) * 5/9) + ' centigrade';
-    newStory = newStory.replace('300 pounds', weight);
-    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
 
 
-  }
+    if (customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replace('Bob', name);
 
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
+    }
+
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300 / 14) + ' stone';
+        const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 fahrenheit', temperature);
+
+
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
